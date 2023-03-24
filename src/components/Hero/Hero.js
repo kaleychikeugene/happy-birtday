b12c83a3dab21d4c666985f9601b19a6e14756cf
Yauheni Kaleichyk <ykaleichyk@piesoft.us>
2023-03-24 17:41:15 +0400

    modify;

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,44 +1,31 @@
 import React from 'react';
 import * as styles from './Hero.module.css';
 import Button from '../Button';
-import { Link } from 'gatsby';
+import {Link} from 'gatsby';
 
 const Hero = (props) => {
-  const {
-    title,
-    subtitle,
-    ctaText,
-    ctaAction,
-    image,
-    maxWidth,
-    ctaStyle,
-    ctaLink,
-    ctaTo,
-    header,
-  } = props;
-  return (
-    <div className={styles.root} style={{ backgroundImage: `url(${image})` }}>
-      <div className={styles.content} style={{ maxWidth: maxWidth }}>
-        {header && <span className={styles.header}>{header}</span>}
-        {title && <h2 className={styles.title}>{title}</h2>}
-        {subtitle && <span className={styles.subtitle}>{subtitle}</span>}
-        {ctaText && (
-          <Button
-            className={`${styles.ctaButton} ${ctaStyle}`}
-            level={'primary'}
-            onClick={ctaAction}
-          >
-            {ctaText}
-          </Button>
-        )}
-        {ctaLink && (
-          <Link className={styles.ctaLink} to={ctaTo}>
-            {ctaLink}
-          </Link>
-        )}
-      </div>
-    </div>
-  );
+    const {
+        title,
+        subtitle,
+        ctaText,
+        ctaAction,
+        image,
+        maxWidth,
+        ctaStyle,
+        ctaLink,
+        ctaTo,
+        header,
+    } = props;
+    return (
+        <div className={styles.root}>
+            <div className={styles.content} style={{width: '90%'}}>
+                <iframe width="100%" height="600" src="https://www.youtube.com/embed/m0IiD0IufdY"
+                        title="Поздравление из Африки для подруги от африканцев" frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen></iframe>
+            </div>
+        </div>
+    );
 };
 
 export default Hero;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -12,21 +12,6 @@ import './Globals.css';
 const Layout = ({ props, children, disablePaddingBottom = false }) => {
   return (
     <>
-      <Helmet>
-        {/* Add any sitewide scripts here */}
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charset="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-      </Helmet>
-
       <Header />
       <main
         className={`${styles.main} ${
@@ -35,7 +20,6 @@ const Layout = ({ props, children, disablePaddingBottom = false }) => {
       >
         {children}
       </main>
-      <Footer />
     </>
   );
 };
