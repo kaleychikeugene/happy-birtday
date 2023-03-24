cc46d47d464790e03eeae04e617401d99ca29deb
Yauheni Kaleichyk <ykaleichyk@piesoft.us>
2023-03-24 18:22:03 +0400

    modify;

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,10 +1,5 @@
 describe('sample render test', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
-  it('displays the title text', () => {
-    cy.get('h2')
-    .contains('Essentials for a cold winter');
-  })
-}) 
+    beforeEach(() => {
+        cy.visit('/')
+    })
+})
